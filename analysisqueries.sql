e83558e63abf9289fdf0da164ea053ca6d5df95f
Omkar Bhujbalrao <141210230+OmkarBhujbalra0@users.noreply.github.com>
2026-03-01 16:57:22 +0530

    Add files via upload

--- analysisqueries.sql
+++ analysisqueries.sql
@@ -1,4 +1,5 @@
 use pokedb;
+
 -- Generations based on their avgstats
 select p.generation,avg(m.total_stats) as avg_total_stats,rank() over (order by avg(total_stats) desc)
 from pokemon_table p
@@ -17,9 +18,70 @@ on p.pokemon_id = m.pokemon_id) as ranktable
 where rankpergen <= 3
 order by avgstats desc;
 
-select count(*),total_stats,avg_stats,generation from (select p.pokemon_name,p.generation,total_stats,avg(total_stats) over (partition by p.generation) as avg_stats
+-- Pokemons whose total_stats are greater than avg_total_stats of their generation
+create view aboveavg as
+select generation,avg_stats_per_gen,count(*) as avgcount from (
+select p.pokemon_name,p.generation,total_stats,
+avg(total_stats) over (partition by generation) as avg_stats_per_gen
 from pokemon_table p
 join metrics_table m
 on p.pokemon_id = m.pokemon_id) as avgtable
-group by generation
-having total_stats > avg_stats;
+where total_stats > avg_stats_per_gen
+group by generation,avg_stats_per_gen;
+
+drop view aboveavg;
+
+create view countview as
+select count(*) as total_count,generation 
+from pokemon_table
+group by generation;
+
+drop view countview;
+
+select * from countview;
+
+select ab.generation,ab.avgcount,ab.avgcount*100/total_count as percentage
+from aboveavg ab
+join countview co
+on ab.generation = co.generation;
+
+-- Average Offensive & Defensive Score of pokemon across generations
+
+create view offdef as 
+select pokemon_name,generation,
+(offensive_score)/2 as offscore,
+(defensive_score)/3 as defscore
+from pokemon_table p
+join metrics_table m
+on p.pokemon_id = m.pokemon_id; 
+
+select generation,avg(offscore) as avg_offscore,
+avg(defscore) as avg_defscore
+from offdef
+group by generation;
+
+-- Standard Deviation of Offensive Score & Defensive Score to check 
+-- whether the score is concentrated in few pokemon or spread across all pokemons.
+
+select generation,std(offscore) as std_offscore,
+std(defscore) as std_defscore
+from offdef
+group by generation;
+
+with offelite as (
+select offscore,generation,pokemon_name,avg(offscore) over (partition by generation) as gen_avg,std(offscore) over (partition by generation) as gen_std,
+(avg(offscore) over (partition by generation) + std(offscore) over (partition by generation)) as sum_off
+from offdef),
+flagged as (
+select generation,
+case
+when offscore > sum_off then 1
+else 0
+end as iselite
+from offelite
+)
+select generation,count(*) as total_count,sum(iselite) as elitecount,
+round(100*sum(iselite)/count(*),2) as percentelite 
+from flagged
+group by generation;
+
