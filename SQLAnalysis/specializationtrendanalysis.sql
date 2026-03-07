8a9d6e8ec1d63a52d48be5552985f0112a8b8843
Omkar Bhujbalrao <141210230+OmkarBhujbalra0@users.noreply.github.com>
2026-03-07 16:24:47 +0530

    Add files via upload

--- SQLAnalysis/specializationtrendanalysis.sql
+++ SQLAnalysis/specializationtrendanalysis.sql
@@ -16,3 +16,71 @@ from pokemon_table p
 join metrics_table m
 on p.pokemon_id = m.pokemon_id) as special_table
 where top_specialized<=10;
+
+-- Average Stats across the generations
+
+select p.generation,avg(s.hp) as avghp,avg(s.attack) as avgatk,avg(s.defense) as avgdef,
+avg(s.sp_atk) as avgspatk,avg(sp_def) as avgspdef,
+avg(speed) as avgspd,count(*) as pokecount
+from pokemon_table p
+join stats_table s
+on p.pokemon_id = s.pokemon_id
+group by generation;
+
+-- Avg. HP increases steadily across the generations
+
+-- Avg. Attack increases overtime,gen 7-9 being highest indicating generations
+-- favors the attack stats
+
+-- Avg. Defense increases across generations
+
+-- Sp.Atk increases after gen 3 due splitting special stats into sp.atk and spdef
+
+-- Speed decreased in early generations but increased again in recent generation
+
+-- Are pokemon types linked with any specific stats
+
+create view typestats as 
+(select p.pokemon_name,p.generation,t.type_name,s.hp,
+s.attack,s.defense,s.sp_atk,s.sp_def,s.speed 
+from pokemon_table p
+join pokemon_types pt
+on p.pokemon_id = pt.pokemon_id
+join types_table t
+on pt.type_id = t.type_id
+join stats_table s
+on p.pokemon_id = s.pokemon_id);
+
+select type_name as type,avg(hp),avg(attack),avg(defense),
+avg(sp_atk),avg(sp_def),avg(speed),
+rank() over (order by avg(hp) desc) as TankRank,
+rank() over (order by avg(attack) desc) as AtkRank,
+rank() over (order by avg(defense) desc) as DefRank,
+rank() over (order by avg(sp_atk) desc) as SpatkRank,
+rank() over (order by avg(sp_def) desc) as SpdefRank,
+rank() over (order by avg(speed) desc) as SpeedRank
+from typestats
+group by type_name
+order by SpdefRank;
+
+-- Flying Type is Specialized in Speed
+-- Electric is specialized in both speed and sp_atk making it fast special attacker
+-- Dragon is Specializes in most of the stats making it most durable ones
+-- Fighting Type is highly specialized in Attack Stats making it Physical Attacker
+-- Steel Type is highly specialized in Defense Stats making it physical tank
+-- Psychic Type are specialized in both special attack and defense stats making it special attacker
+-- Fairy Type are specialized in Special Defense stat making special tank
+
+-- Are Legendaries/Mythical/Normal Pokemons specialized or balanced in stats
+
+select pd.category,avg(s.hp) as avghp,avg(s.attack) as avgatk,avg(s.defense) as avgdef,
+avg(s.sp_atk) as avgspatk,avg(s.sp_def) as avgdpdef,avg(s.speed) as avgspd 
+from pokedistinct pd
+join stats_table s
+on pd.pokemon_id = s.pokemon_id
+group by pd.category; 
+
+-- Normal Pokemons have stats in balanced manner
+-- Mythical pokemons are slightly leaning towards Special Attack
+-- Legendary pokemons are stronger on overall stats 
+
